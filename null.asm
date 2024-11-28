c6b577543791f519ec374860f6bdc9f37e3f8793
REVAN <joseaguero11@estudiantec.cr>
2024-11-27 19:59:20 -0600

    fix: lexer table fixed

---
+++ null.asm
@@ -0,0 +1,28 @@
+; Declare variable x
+x:	RESW 1
+
+
+
+; Assign y = z
+MOV AX, z
+MOV y, AX
+
+
+
+; Declare variable funcion
+funcion:	RESW 1
+
+; Declare variable a
+a:	RESW 1
+
+; Declare variable a
+a:	RESW 1
+
+
+; Declare variable y
+y:	RESW 1
+
+; Assign y = z
+MOV AX, z
+MOV y, AX
+
